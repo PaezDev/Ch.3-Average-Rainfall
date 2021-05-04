a5680b519bcec003a74ad2ce50d7545ef09b9b53
Andry Paez <andryypaez@gmail.com>
2021-05-03 23:31:22 -0700

    Initial Commit

---
+++ Ch.3 Average Rainfall/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ch.3 Average Rainfall
+//
+//  Created by Andry Paez on 5/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
