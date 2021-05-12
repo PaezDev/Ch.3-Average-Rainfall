b01bf57b5bbd4a636b3b9d2184fcf192d773b2de
PaezDev <andryypaez@gmail.com>
2021-05-12 01:00:35 -0700

    Update main.cpp

--- Ch.3 Average Rainfall/main.cpp
+++ Ch.3 Average Rainfall/main.cpp
@@ -1,14 +1,31 @@
-//
-//  main.cpp
-//  Ch.3 Average Rainfall
-//
-//  Created by Andry Paez on 5/3/21.
-//
-
+//  Programming challenge #4 in Ch.3
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main ()
+{
+    string month1, month2, month3;
+    double rain1 = 0, rain2 = 0, rain3 = 0;
+    
+    cout << "Enter a month: " << endl;
+    cin >> month1;
+    cout << "What was the amount of rain (in inches) that fell: " << endl;
+    cin >> rain1;
+    cout << "Enter a month: " << endl;
+    cin >> month2;
+    cout << "What was the amount of rain (in inches) that fell: " << endl;
+    cin >> rain2;
+    cout << "Enter a month: " << endl;
+    cin >> month3;
+    cout << "What was the amount of rain (in inches) that fell: "<< endl;
+    cin >> rain3;
+    
+    double average = (rain1 + rain2 + rain3) / 3;
+    
+    cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3    << " is\n" << average << endl;
     return 0;
+    
+    
+    
 }
